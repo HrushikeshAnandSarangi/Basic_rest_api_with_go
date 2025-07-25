3603861c053a6bcd794e0359d60ad29b788b176b
HrushikeshAnandSarangi <hrushikeshsarangi7@gmail.com>
2025-07-25 17:12:08 +0530

    Final Deployment commit

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/HrushikeshAnandSarangi/go-rest/config"
 	"github.com/HrushikeshAnandSarangi/go-rest/routes"
 	"github.com/gin-gonic/gin"
@@ -10,5 +13,16 @@ func main() {
 	r := gin.Default()
 	config.ConnectDatabase()
 	routes.AuthRoutes(r)
-	r.Run(":8000")
+
+	// Get port from environment (Render sets this)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000" // fallback for local
+	}
+
+	log.Printf("Listening on port %s...\n", port)
+	err := r.Run(":" + port)
+	if err != nil {
+		log.Fatalf("Failed to run server: %v", err)
+	}
 }
